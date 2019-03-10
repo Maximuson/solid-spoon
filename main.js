d675ae033c614372e86abadc082772ee4cd81c19
Maximuson <maxpavlenko54@gmail.com>
2019-03-10 17:45:52 +0200

    Add 3 circle with procents

---
+++ main.js
@@ -0,0 +1,9 @@
+// let imgs3Section = document.querySelector(".portfolio-3photos")
+// let img2 = document.querySelector( ".portfolio-3image_2" )
+//  img2.addEventListener("mouseover", (e)=>{
+// 	imgs3Section.style["gridTemplateColumns"]=" 1fr 2fr 1fr"
+	
+// } )
+// img2.addEventListener('mouseout', ()=>{
+// 	imgs3Section.style["gridTemplateColumns"]=" 1fr 1fr 1fr"
+// })
